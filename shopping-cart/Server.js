dc6edb99232883f7729e54913ade1af3b99345af
keerthivasan2211 <155253910+keerthivasan2211@users.noreply.github.com>
2025-09-09 10:01:26 +0530

    server link change

--- shopping-cart/Server.js
+++ shopping-cart/Server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "http://10.70.100.48:5173",
+  origin: "http://vedi2-backend.onrender.com",
   credentials: true,
 }));
 app.use(bodyParser.json());
